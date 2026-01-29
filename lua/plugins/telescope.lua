ffef6184546aa99c15b5b545d52411877ba3fdc6
ruohao <ruohaolin@gmail.com>
2026-01-28 23:57:27 -0500

    telescope

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,31 @@
+return {
+	{
+		"nvim-telescope/telescope.nvim",
+		version = "*",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+			-- Media preview
+			{ "nvim-telescope/telescope-media-files.nvim" },
+			{ "nvim-lua/popup.nvim" },
+		},
+		config = function()
+			require("telescope").load_extension("media_files")
+			require("telescope").setup({
+				defaults = {
+					layout_config = { prompt_position = "top" },
+					sorting_strategy = "ascending",
+				},
+				extensions = {
+					media_files = {
+						-- filetypes whitelist
+						-- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
+						filetypes = { "png", "webp", "jpg", "jpeg" },
+						-- find command (defaults to `fd`)
+						find_cmd = "rg",
+					},
+				},
+			})
+		end,
+	},
+}
