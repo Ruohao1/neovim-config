7d2411ad46da4d07aed66a96752ec38aa7a8bc20
ruohao <ruohaolin@gmail.com>
2026-01-28 20:11:05 -0500

    basic options

---
+++ lua/config/options.lua
@@ -0,0 +1,40 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn = "yes"
+vim.opt.cursorline = true
+
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.termguicolors = true
+vim.opt.updatetime = 200
+vim.opt.timeoutlen = 400
+
+vim.opt.clipboard = "unnamedplus"
+
+-- Better splits
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+-- Undo history
+vim.opt.undofile = true
+
+-- Diagnostics UI
+vim.diagnostic.config({
+  virtual_text = true,
+  severity_sort = true,
+  float = { border = "rounded" },
+})
+
