6d7d919e6367dcd91466a513875106722d28d13e
ruohao <ruohaolin@gmail.com>
2026-01-31 17:32:00 -0500

    no cycle tmux panes navigation

--- lua/plugins/tmux.lua
+++ lua/plugins/tmux.lua
@@ -44,7 +44,7 @@ return {
 			},
 			navigation = {
 				-- cycles to opposite pane while navigating into the border
-				cycle_navigation = true,
+				cycle_navigation = false,
 
 				-- enables default keybindings (C-hjkl) for normal mode
 				enable_default_keybindings = false,
