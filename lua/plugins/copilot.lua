4f446ba1c34c28c5c71aa65a4d609044fdb047cf
ruohao <ruohaolin@gmail.com>
2026-02-02 12:41:45 -0500

    copilot

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,86 @@
+return {
+	"zbirenbaum/copilot.lua",
+	cmd = "Copilot",
+	event = "InsertEnter",
+	config = function()
+		require("copilot").setup({
+			panel = {
+				enabled = false,
+				-- auto_refresh = false,
+				-- keymap = {
+				-- 	jump_prev = "[[",
+				-- 	jump_next = "]]",
+				-- 	accept = "<CR>",
+				-- 	refresh = "gr",
+				-- 	open = "<M-CR>",
+				-- },
+				-- layout = {
+				-- 	position = "bottom", -- | top | left | right | bottom |
+				-- 	ratio = 0.4,
+				-- },
+			},
+			suggestion = {
+				enabled = true,
+				auto_trigger = false,
+				hide_during_completion = true,
+				debounce = 15,
+				trigger_on_accept = true,
+				keymap = {
+					accept = "<C-l>",
+					accept_word = "<C-k>",
+					accept_line = "<C-j>",
+					next = "<C-n>",
+					prev = "<C-p>",
+					dismiss = "<C-d>",
+					toggle_auto_trigger = false,
+				},
+			},
+			nes = {
+				enabled = false, -- requires copilot-lsp as a dependency
+				-- auto_trigger = false,
+				-- keymap = {
+				-- 	accept_and_goto = false,
+				-- 	accept = false,
+				-- 	dismiss = false,
+				-- },
+			},
+			auth_provider_url = nil, -- URL to authentication provider, if not "https://github.com/"
+			logger = {
+				file = vim.fn.stdpath("log") .. "/copilot-lua.log",
+				file_log_level = vim.log.levels.OFF,
+				print_log_level = vim.log.levels.WARN,
+				trace_lsp = "off", -- "off" | "debug" | "verbose"
+				trace_lsp_progress = false,
+				log_lsp_messages = false,
+			},
+			copilot_node_command = "node", -- Node.js version must be > 22
+			workspace_folders = {},
+			copilot_model = "",
+			disable_limit_reached_message = false, -- Set to `true` to suppress completion limit reached popup
+			root_dir = function()
+				return vim.fs.dirname(vim.fs.find(".git", { upward = true })[1])
+			end,
+			should_attach = function(_, _)
+				if not vim.bo.buflisted then
+					vim.notify("not attaching, buffer is not 'buflisted'")
+					return false
+				end
+
+				if vim.bo.buftype ~= "" then
+					vim.notify("not attaching, buffer 'buftype' is " .. vim.bo.buftype)
+					return false
+				end
+
+				return true
+			end,
+			server = {
+				type = "nodejs", -- "nodejs" | "binary"
+				custom_server_filepath = nil,
+			},
+			server_opts_overrides = {},
+		})
+	end,
+	vim.keymap.set("n", "<Leader>cc", function()
+		require("copilot.suggestion").toggle_auto_trigger()
+	end, { desc = "Toggle Copliot" }),
+}
