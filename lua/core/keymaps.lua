13d392ac450d393e4a44f1c2fe7465e2fc1b7314
ruohao <ruohaolin@gmail.com>
2026-02-02 12:41:00 -0500

    life quality file writing

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -6,6 +6,11 @@ vim.g.maplocalleader = " "
 
 -- Clear search highlight
 map("n", "<leader>h", "<cmd>nohlsearch<cr>", opts)
+vim.api.nvim_create_user_command("W", "w", {})
+vim.api.nvim_create_user_command("Wq", "wq", {})
+vim.api.nvim_create_user_command("WQ", "wq", {})
+vim.api.nvim_create_user_command("Qa", "qa", {})
+vim.api.nvim_create_user_command("QA", "qa", {})
 
 -- Better motion
 map("i", "jj", "<Esc>", opts)
